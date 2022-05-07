3cd775256101e683ce4f4762d37605477f41ebfb
Evanflow89 <ivanbaragone@gmail.com>
2022-05-07 10:25:53 +0200

    ciclo while's not working

---
+++ parieDispari/main.js
@@ -0,0 +1,30 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+let userChoose = prompt("Scegli tra pari e dispari");
+
+// console.log(userChoose)
+
+let userNum = Number(prompt("Inserisci un numero tra 1 e 5"));
+
+
+let oddEven = [];
+const pari = pari;
+const dispari = dispari;
+
+
+while ((userChoose != pari) || (userChoose != dispari)) {
+    userChoose = prompt("Scegli tra pari e dispari");
+} oddEven.push(userChoose);
+
+console.log(oddEven);
+
+
+while (userNum > 5 || userNum < 1 || isNaN(userNum) == true) {
+    userNum = Number(prompt("Inserisci un numero tra 1 e 5"));
+}
+console.log(`Hai scelto il numero ${userNum}`);
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
